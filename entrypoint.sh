0dd807921ae079b838da8fbaaea4f6882408f2d5
pugang <pugang@nibirutech.com>
2017-11-09 13:31:41 +0800

    修复判断空白仓库失败的问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -x
 sed -i "s/IP_OR_HOST/$HOST/" /etc/nginx/conf.d/hexo.conf
 pm2 start webhook.js --name webhook
 
-[ ! -d "/app/hexo" ] \
+[ ! -d "/app/hexo/public" ] \
 	&& git clone $GITREPO /app/hexo/ \
 	&& cd /app/hexo \
 	&& npm install 
